22166b4b2d253337eab6c73434f7eeb0f14013ce
alfisyahrinn <sahrin201@gmail.com>
2024-02-11 18:48:28 -0800

    feat(font) add font dancing and cormorant

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="p-40 text-4xl font-cormorant">Hello World</h1>
+    </div>
+  );
+}                                   
