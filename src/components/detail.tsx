0bc0b7cde242d27944e8296b316c989297082887
alfisyahrinn <sahrin201@gmail.com>
2024-02-16 21:17:42 +0700

    fix(detail) fix  to &quot

--- src/components/detail.tsx
+++ src/components/detail.tsx
@@ -37,50 +37,163 @@ export default function DetailSection() {
         <div className="flex flex-col gap-2">
           <p className="text-xl font-cormorant text-primary mt-5">Resepsi</p>
           <img src="assets/image/banner/accent.png" alt="accent" />
-          <p className="text-xl font-bold font-cormorant text-primary">Selasa, 30 April 2024</p>
+          <p className="text-xl font-bold font-cormorant text-primary">
+            Selasa, 30 April 2024
+          </p>
           <img src="assets/image/banner/accent.png" alt="accent" />
         </div>
       </div>
-        <div className="mt-11 text-center">
-          <p className="text-xl font-cormorant font-bold text-secondary underline underline-offset-8">Kami Yang Berbahagia</p>
-          <div className="flex justify-around mt-7"> 
-            <p className="text-[15px] max-w-[159px] font-cormorant text-primary text-left">Kel. Bpk. Drs. M. Umar Ar (Alm) & Ibu Asnaini</p>
-            <p className="text-[15px]  max-w-[107px] font-cormorant text-primary text-right">Kel. Bpk. Ifnel & Ibu Irdawati</p>
-          </div>
-          <div className="mt-5">
-            <h1 className='text-[40px] font-dancing text-primary'>Arief & Mia</h1>
-          </div>
+      <div className="mt-11 text-center">
+        <p className="text-xl font-cormorant font-bold text-secondary underline underline-offset-8">
+          Kami Yang Berbahagia
+        </p>
+        <div className="flex justify-around mt-7">
+          <p className="text-[15px] max-w-[159px] font-cormorant text-primary text-left">
+            Kel. Bpk. Drs. M. Umar Ar (Alm) & Ibu Asnaini
+          </p>
+          <p className="text-[15px]  max-w-[107px] font-cormorant text-primary text-right">
+            Kel. Bpk. Ifnel & Ibu Irdawati
+          </p>
         </div>
-        <div className="mt-7 text-center">
-          <p className="text-xl font-cormorant font-bold text-secondary underline underline-offset-8">Turut Mengundang</p>
-          <ul className="mt-5 ">
-            <li className="text-[16px] font-cormorant text-primary">Munawardi (Adie)/ Rizqia Ade Ulisma (Kiki)</li>
-            <li className="text-[16px] font-cormorant text-primary">Afrizal (Izal)/Rahmawati (Amah)</li>
-            <li className="text-[16px] font-cormorant text-primary">Nikmah</li>
-            <li className="text-[16px] font-cormorant text-primary">Alfi Syahrin</li>
-          </ul>
+        <div className="mt-5">
+          <h1 className="text-[40px] font-dancing text-primary">Arief & Mia</h1>
         </div>
-        <div className="mt-24 relative">
-          <div>
-            <svg className="-top-[50px] -z-10 absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8B7A4E" fill-opacity="1" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-            <svg className="-top-[60px] -z-10 absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8B7A4E" fill-opacity="0.5" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-            <svg className="-top-[50px] left-36 -z-10 absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8B7A4E" fill-opacity="0.6" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-            <svg className="-top-[66px] left-36 -z-10 absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8B7A4E" fill-opacity="0.6" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-            <svg className="-top-[66px] -left-28 -z-10 absolute" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8B7A4E" fill-opacity="0.6" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-          </div>
-          <div className="py-3 bg-[#8B7A4E]  flex-col flex w-full justify-center items-center text-center">
-          <p className="text-[15px] max-w-[345px] font-cormorant text-white">"Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan untukmu isteri-isteri dari jenismu sendiri, supaya kamu cenderung dan merasa tenteram kepada-Nya, dan dijadikan-Nya diantaramu rasa kasih dan sayang. Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda bagi kaum yang berfikir."</p>
-          <p className="mt-2  text-[17px] max-w-[345px] font-dancing text-white">Q.S. Ar-Rum : 21</p>
-          </div>
-          <div>
-              <svg className="-bottom-[73px] absolute -z-10 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8B7A4E" fill-opacity="1" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
-              <svg className="-bottom-[83px] absolute -z-10 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8B7A4E" fill-opacity="0.5" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
-              <svg className="-bottom-[74px] right-28 absolute -z-10 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8B7A4E" fill-opacity="0.5" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
-              {/* <svg className=" -left-60 absolute -z-10 w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="0.5" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg> */}
-              <svg className="-bottom-[74px] absolute -z-20 w-[499px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#8B7A4E" fill-opacity="0.5" d="M0,256L40,245.3C80,235,160,213,240,224C320,235,400,277,480,266.7C560,256,640,192,720,170.7C800,149,880,171,960,197.3C1040,224,1120,256,1200,224C1280,192,1360,96,1400,48L1440,0L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path></svg>
-          </div>
+      </div>
+      <div className="mt-7 text-center">
+        <p className="text-xl font-cormorant font-bold text-secondary underline underline-offset-8">
+          Turut Mengundang
+        </p>
+        <ul className="mt-5 ">
+          <li className="text-[16px] font-cormorant text-primary">
+            Munawardi (Adie)/ Rizqia Ade Ulisma (Kiki)
+          </li>
+          <li className="text-[16px] font-cormorant text-primary">
+            Afrizal (Izal)/Rahmawati (Amah)
+          </li>
+          <li className="text-[16px] font-cormorant text-primary">Nikmah</li>
+          <li className="text-[16px] font-cormorant text-primary">
+            Alfi Syahrin
+          </li>
+        </ul>
+      </div>
+      <div className="mt-24 relative">
+        <div>
+          <svg
+            className="-top-[50px] -z-10 absolute"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#8B7A4E"
+              fill-opacity="1"
+              d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            ></path>
+          </svg>
+          <svg
+            className="-top-[60px] -z-10 absolute"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#8B7A4E"
+              fill-opacity="0.5"
+              d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            ></path>
+          </svg>
+          <svg
+            className="-top-[50px] left-36 -z-10 absolute"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#8B7A4E"
+              fill-opacity="0.6"
+              d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            ></path>
+          </svg>
+          <svg
+            className="-top-[66px] left-36 -z-10 absolute"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#8B7A4E"
+              fill-opacity="0.6"
+              d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            ></path>
+          </svg>
+          <svg
+            className="-top-[66px] -left-28 -z-10 absolute"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#8B7A4E"
+              fill-opacity="0.6"
+              d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            ></path>
+          </svg>
         </div>
-      
+        <div className="py-3   bg-[#8B7A4E]  flex-col flex w-full justify-center items-center text-center">
+          <p className="text-[15px] max-w-[345px] font-cormorant text-white">
+            "Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan
+            untukmu isteri-isteri dari jenismu sendiri, supaya kamu cenderung
+            dan merasa tenteram kepada-Nya, dan dijadikan-Nya diantaramu rasa
+            kasih dan sayang. Sesungguhnya pada yang demikian itu benar-benar
+            terdapat tanda-tanda bagi kaum yang berfikir."
+          </p>
+          <p className="mt-2  text-[17px] max-w-[345px] font-dancing text-white">
+            Q.S. Ar-Rum : 21
+          </p>
+        </div>
+        <div>
+          <svg
+            className="-bottom-[73px] absolute -z-10 "
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#8B7A4E"
+              fill-opacity="1"
+              d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+            ></path>
+          </svg>
+          <svg
+            className="-bottom-[83px] absolute -z-10 "
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#8B7A4E"
+              fill-opacity="0.5"
+              d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+            ></path>
+          </svg>
+          <svg
+            className="-bottom-[74px] right-28 absolute -z-10 "
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#8B7A4E"
+              fill-opacity="0.5"
+              d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
+            ></path>
+          </svg>
+          {/* <svg className=" -left-60 absolute -z-10 w-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="0.5" d="M0,192L80,160C160,128,320,64,480,64C640,64,800,128,960,138.7C1120,149,1280,107,1360,85.3L1440,64L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg> */}
+          <svg
+            className="-bottom-[74px] absolute -z-20 w-[499px]"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#8B7A4E"
+              fill-opacity="0.5"
+              d="M0,256L40,245.3C80,235,160,213,240,224C320,235,400,277,480,266.7C560,256,640,192,720,170.7C800,149,880,171,960,197.3C1040,224,1120,256,1200,224C1280,192,1360,96,1400,48L1440,0L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
+            ></path>
+          </svg>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/splash.tsx
+++ src/components/splash.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function Splash() {
   return (
-    <div className='flex flex-col relative justify-center items-center h-[100vh] text-center gap-4 '>
-      <img className='absolute top-0 left-0' src="assets/image/banner/banner-top.png" alt="decoration" />
-      <img className='absolute bottom-0 right-0' src="assets/image/banner/banner-bottom.png" alt="decoration" />
-      <p className='text-2xl font-dancing text-secondary'>The Wedding of</p>
-      <h1 className='text-8xl font-dancing text-primary'>Arief & Mia</h1>
-      <p className='text-2xl font-medium text-secondary font-cormorant'>Kepada Bpk/Ibu/Saudara/i:</p>
-      <p className='text-2xl text-secondary font-cormorant'>Hafshah dan Meriam</p>
-      <button className='bg-primary px-[18px] py-[8px] flex gap-2 text-white font-cormorant text-2xl'><img src="assets/image/icon/email.svg" alt="open" />Buka Undangan</button>
+    <div className="flex flex-col relative justify-center items-center h-[100vh] text-center gap-4 ">
+      <img
+        className="absolute top-0 left-0"
+        src="assets/image/banner/banner-top.png"
+        alt="decoration"
+      />
+      <img
+        className="absolute bottom-0 right-0"
+        src="assets/image/banner/banner-bottom.png"
+        alt="decoration"
+      />
+      <p className="text-2xl font-dancing text-secondary">The Wedding of</p>
+      <h1 className="text-8xl font-dancing text-primary">Arief & Mia</h1>
+      <p className="text-2xl font-medium text-secondary font-cormorant">
+        Kepada Bpk/Ibu/Saudara/i:
+      </p>
+      <p className="text-2xl text-secondary font-cormorant">
+        Hafshah dan Meriam
+      </p>
+      <button className="bg-primary px-[18px] py-[8px] flex gap-2 text-white font-cormorant text-2xl">
+        <img src="assets/image/icon/email.svg" alt="open" />
+        Buka Undangan
+      </button>
     </div>
-  )
+  );
 }
