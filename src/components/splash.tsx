0699864be2348a0cc1cef55621c7f219904e9b4a
Alfi Syahrin <sahrin201@gmail.com>
2024-02-23 00:31:28 +0700

    add musik

--- src/components/splash.tsx
+++ src/components/splash.tsx
@@ -1,16 +1,25 @@
 "use client";
-import React from "react";
+import React, { useState, useRef } from "react";
+import { FaCirclePause } from "react-icons/fa6";
 
 export default function Splash() {
+  const [musik, setMusik] = useState(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
   const handleClick = () => {
+    handleMusik()
+    // setMusik(true);
+
+    console.log({ musik });
+
     if (typeof window !== "undefined") {
       window.onscroll = function () {};
 
       const rootElement: HTMLElement | null = document.querySelector(":root");
       if (rootElement) {
         rootElement.style.scrollBehavior = "smooth";
-        rootElement.style.overflowX = 'hidden'
-        rootElement.style.overflow = 'auto'
+        rootElement.style.overflowX = "hidden";
+        rootElement.style.overflow = "auto";
       }
       const detailSection = document.getElementById("detail");
       if (detailSection) {
@@ -18,37 +27,59 @@ export default function Splash() {
       }
     }
   };
+
+  const handleMusik = () => {
+    setMusik(!musik);
+    if (audioRef.current) {
+      if (musik) {
+        audioRef.current.pause(); // Jeda musik jika sedang diputar
+      } else {
+        audioRef.current.play(); // Putar musik jika sedang dijeda
+      }
+    }
+  };
+
   return (
-    <div className="flex flex-col relative justify-center items-center h-[100vh] text-center gap-4 ">
-      <img
-        className="absolute top-0 left-0  animate-fade animate-duration-1000 animate-delay-50"
-        src="assets/image/banner/banner-top.png"
-        alt="decoration"
-      />
-      <img
-        className="absolute bottom-0 right-0 -z-10  animate-fade animate-duration-1000 animate-delay-50"
-        src="assets/image/banner/banner-bottom.png"
-        alt="decoration"
-      />
-      <p className="text-2xl font-dancing text-secondary animate-fade animate-once animate-duration-1000 animate-delay-100">
-        The Wedding of
-      </p>
-      <h1 className="text-8xl font-dancing text-primary animate-fade animate-once animate-duration-1000 animate-delay-200">
-        Arief & Mia
-      </h1>
-      <p className="text-2xl font-medium text-secondary font-cormorant animate-fade animate-once animate-duration-1000 animate-delay-300">
-        Kepada Bpk/Ibu/Saudara/i:
-      </p>
-      <p className="text-2xl text-secondary font-cormorant animate-fade animate-once animate-duration-1000 animate-delay-400">
-        Hafshah dan Meriam
-      </p>
-      <button
-        onClick={handleClick}
-        className="animate-fade animate-once animate-duration-1000 animate-delay-500 bg-primary px-[18px] py-[8px] flex gap-2 text-white font-cormorant text-2xl"
-      >
-        <img src="assets/image/icon/email.svg" alt="open" />
-        Buka Undangan
-      </button>
-    </div>
+    <>
+      <div className="flex flex-col relative justify-center items-center h-[100vh] text-center gap-4 ">
+        <img
+          className="absolute top-0 left-0  animate-fade animate-duration-1000 animate-delay-50"
+          src="assets/image/banner/banner-top.png"
+          alt="decoration"
+        />
+        <img
+          className="absolute bottom-0 right-0 -z-10  animate-fade animate-duration-1000 animate-delay-50"
+          src="assets/image/banner/banner-bottom.png"
+          alt="decoration"
+        />
+        <p className="text-2xl font-dancing text-secondary animate-fade animate-once animate-duration-1000 animate-delay-100">
+          The Wedding of
+        </p>
+        <h1 className="text-8xl font-dancing text-primary animate-fade animate-once animate-duration-1000 animate-delay-200">
+          Arief & Mia
+        </h1>
+        <p className="text-2xl font-medium text-secondary font-cormorant animate-fade animate-once animate-duration-1000 animate-delay-300">
+          Kepada Bpk/Ibu/Saudara/i:
+        </p>
+        <p className="text-2xl text-secondary font-cormorant animate-fade animate-once animate-duration-1000 animate-delay-400">
+          Hafshah dan Meriam
+        </p>
+        <button
+          onClick={handleClick}
+          className="animate-fade animate-once animate-duration-1000 animate-delay-500 bg-primary px-[18px] py-[8px] flex gap-2 text-white font-cormorant text-2xl"
+        >
+          <img src="assets/image/icon/email.svg" alt="open" />
+          Buka Undangan
+        </button>
+        {!musik && (
+          <audio ref={audioRef} autoPlay loop>
+            <source src="./assets/musik-arab.mp3" type="audio/mp3" />
+          </audio>
+        )}
+      </div>
+      <div className="absolute bottom-0 right-0 pb-6 pe-6">
+        <FaCirclePause onClick={handleMusik} className="h-9 w-9 text-primary" />
+      </div>
+    </>
   );
 }
